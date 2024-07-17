c33e187b341f5d2e6fb534f980848f6e80e9cfef
setsuka77 <ganbal9981@gmail.com>
2024-07-17 11:43:28 +0900

    add 大塚~AttendanceDto.java fix 大塚~record.html,AttendanceControllerの修正

--- src/main/java/com/example/demo/controller/AttendanceController.java
+++ src/main/java/com/example/demo/controller/AttendanceController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
@@ -43,8 +45,7 @@ public class AttendanceController {
 	 * @param model
 	 * @return 勤怠登録画面
 	 */
- 
-	@PostMapping("/attendance/show")
+	@PostMapping("/attendance")
 	public String showAttendance(@RequestParam("year") int year,
 			@RequestParam("month") int month,
 			Model model) {
@@ -61,5 +62,32 @@ public class AttendanceController {
 		model.addAttribute("selectedMonth", year + "年" + String.format("%02d", month) + "月");
 		
 		return "attendance/record"; // 表示するテンプレート名
+	}
+	
+	/*
+	 * 「登録」ボタン押下
+	 * @param model
+	 * @return 勤怠登録画面
+	 */
+	
+	@RequestMapping(path = "/attendance", params = "regist", method = RequestMethod.POST)
+	public String regist(Model model) {
+		
+		
+		return "attendance/record";
+	}
+	
+	
+	
+	/*
+	 * 「登録申請」ボタン押下
+	 * @param model
+	 * @return 勤怠登録画面
+	 */
+	@RequestMapping(path = "/attendance", params = "request", method = RequestMethod.POST)
+	public String request(Model model) {
+		
+		
+		return "attendance/record";
 	}
 }
