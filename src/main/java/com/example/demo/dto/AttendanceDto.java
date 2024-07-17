63c6a0c1429dbe6b3d198805e60f4bf3937b5294
setsuka77 <ganbal9981@gmail.com>
2024-07-17 11:43:53 +0900

    add 大塚~DTO追加

---
+++ src/main/java/com/example/demo/dto/AttendanceDto.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class AttendanceDto {
+
+	/** 勤怠ID */
+	private Integer id;
+	/** ユーザーID */
+	private Integer userId;
+	/** 勤務状況 */
+	private Integer status;
+	/**日付 */
+	private Date date;
+	/** 勤務開始時刻 */
+	private String startTime;
+	/** 勤務開始時刻 */
+	private String endTime;
+	/** 備考 */
+	private String remarks;
+
+}
