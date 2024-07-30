3162cc7173a19d54a498e0b48a9836977fb768b5
58M0 <kobayashim@analix.co.jp>
2024-07-30 09:42:43 +0900

    fix:小林　mapper/MonthlyAttendanceReq.xmlのディレクトリ指定修正

--- src/main/java/com/example/demo/mapper/MonthlyAttendanceReqMapper.java
+++ src/main/java/com/example/demo/mapper/MonthlyAttendanceReqMapper.java
@@ -2,10 +2,10 @@ package com.example.demo.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+//import org.apache.ibatis.annotations.Select;
+//import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.dto.MonthlyAttendanceReqDto;
 import com.example.demo.entity.MonthlyAttendanceReq;
@@ -22,41 +22,41 @@ public interface MonthlyAttendanceReqMapper {
 	 * 承認申請ボタン押下
 	 * 承認申請登録
 	 */
-	@Insert("INSERT INTO monthly_attendance_req (user_id, target_year_month, date, status) VALUES (#{userId}, #{targetYearMonth}, #{date}, #{status})")
+//	@Insert("INSERT INTO monthly_attendance_req (user_id, target_year_month, date, status) VALUES (#{userId}, #{targetYearMonth}, #{date}, #{status})")
 	void insert(MonthlyAttendanceReq req);
     
     /*
      * 承認待ちの情報をすべて取得
      */
-    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
-            "FROM monthly_attendance_req mar " +
-            "JOIN users u ON mar.user_id = u.id " +
-            "WHERE mar.status = 1")
+//    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
+//            "FROM monthly_attendance_req mar " +
+//            "JOIN users u ON mar.user_id = u.id " +
+//            "WHERE mar.status = 1")
     List<MonthlyAttendanceReqDto> findAllWithStatus();
     
     /*
      * 申請IDから情報を取得
      */
-    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
-            "FROM monthly_attendance_req mar " +
-            "JOIN users u ON mar.user_id = u.id " +
-            "WHERE mar.id = #{id}")
+//    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
+//            "FROM monthly_attendance_req mar " +
+//            "JOIN users u ON mar.user_id = u.id " +
+//            "WHERE mar.id = #{id}")
     MonthlyAttendanceReqDto findById(Integer id);
     
     /*
      * Idとdateから申請IDを取得する
      */
-    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
-            "FROM monthly_attendance_req mar " +
-            "JOIN users u ON mar.user_id = u.id " +
-            "WHERE mar.user_id = #{userId} AND mar.date = #{firstDate}")
+//    @Select("SELECT u.name AS userName, u.id AS userId, mar.* " +
+//            "FROM monthly_attendance_req mar " +
+//            "JOIN users u ON mar.user_id = u.id " +
+//            "WHERE mar.user_id = #{userId} AND mar.date = #{firstDate}")
     MonthlyAttendanceReqDto findByIdAndDate(Integer userId,java.util.Date firstDate);
     
     
     /*
      * ステータスの更新
      */
-    @Update("UPDATE monthly_attendance_req SET status = #{status} WHERE id = #{id}")
+//    @Update("UPDATE monthly_attendance_req SET status = #{status} WHERE id = #{id}")
     void updateStatus(MonthlyAttendanceReqDto req);
     
     
