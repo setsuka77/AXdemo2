0172c72b1c52799d3071f463f0454ce9f11ca28f
setsuka77 <ganbal9981@gmail.com>
2024-08-05 17:52:04 +0900

    fix 大塚　ハッシュ化機能追加

---
+++ src/main/java/com/example/demo/util/HashUtil.java
@@ -0,0 +1,23 @@
+package com.example.demo.util;
+
+import java.security.MessageDigest;
+
+public class HashUtil {
+    public static String sha256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
