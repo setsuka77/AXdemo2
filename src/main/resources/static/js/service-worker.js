b1a0dfda12bbef7cb12213ce0dcc3de1a6a79d57
58M0 <kobayashim@analix.co.jp>
2024-09-19 17:10:06 +0900

    fix:小林　プッシュ通知実装完了

--- src/main/resources/static/js/service-worker.js
+++ src/main/resources/static/js/service-worker.js
@@ -17,10 +17,22 @@ self.addEventListener('push', function(event) {
     const data = event.data ? event.data.json() : {};
     const options = {
         body: data.body || 'Default body',
-        icon: data.icon || '/default-icon.png'
+        icon: data.icon || '/default-icon.png',
+        data: {
+            url: 'http://localhost:8080/login/index' // 通知クリック時に遷移するURL
+        }
     };
     event.waitUntil(
         self.registration.showNotification(data.title || 'Default title', options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click received.');
+    const url = event.notification.data.url || 'http://localhost:8080/login/index'; // デフォルトのURL
+    event.notification.close(); // 通知を閉じる
+    event.waitUntil(
+        clients.openWindow(url) // URLを開く
     );
 });
 
